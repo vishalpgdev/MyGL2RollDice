1d03b7b1e921b86d8c078dd2f1c275bb5d669252
vishal-parmar <vishal.parmar@pavansgroup.in>
2020-11-27 15:50:09 +0530

    Challenge: Add a second button to the app labeled "Count Up" that appears just below the Roll button. When tapped, the Count Up button should get the current value of the result text view, add 1 to it, and update the text view. Make sure you handle these edge cases:
    If the result text view does not yet contain a number (that is, if the text view still has the default "Hello World" string), set the result text to 1.
    If the number is already 6, do nothing.
    Tip: The string in TextView views is an instance of class CharSequence. To test its value, you need to convert it to a string:
    
    resultText.text.toString()
    
    Coding challenge done solution code

--- app/src/main/java/com/pavansgroup/vishaltraining/mygl2rolldice/MainActivity.kt
+++ app/src/main/java/com/pavansgroup/vishaltraining/mygl2rolldice/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.pavansgroup.vishaltraining.mygl2rolldice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private var okay = "okay"
+    private lateinit var resultText: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        resultText = findViewById(R.id.resultText)
+        val rollButton: Button = findViewById(R.id.btnRoll)
+        val countButton: Button = findViewById(R.id.btnCountUP)
+        rollButton.setOnClickListener { rollDice() }
+        countButton.setOnClickListener {
+            val currenText = resultText.text.toString()
+            if (currenText == "Hello World!") {
+                resultText.text = 1.toString()
+            } else if (currenText == "6") {
+                resultText.text = currenText
+            } else {
+                var c = currenText.toInt()
+                val d = c + 1
+                resultText.text = d.toString()
+            }
+        }
+    }
+
+    private fun rollDice() {
+        //Toast.makeText(this@MainActivity, "Button Clicked.", Toast.LENGTH_SHORT).show()
+        val randomInt = (1..6).random()
+        resultText.text = randomInt.toString()
     }
 }
